f73ce6c8585a175aeebaa0f11b29128fec937978
Jacalz <jacob.alzen@gmail.com>
2020-08-27 12:55:44 +0200

    Finish the inital version of the title screen

---
+++ TitleScreen/Animations/FadeIn.gd
@@ -0,0 +1,10 @@
+extends ColorRect
+
+signal fade_finished
+
+func fade_in():
+	$AnimationPlayer.play("fade_in")
+
+
+func _on_AnimationPlayer_animation_finished(anim_name):
+	emit_signal("fade_finished")

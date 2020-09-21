a97be7bf71fe3716cb8fe96330ccd404ebb48d07
Jacalz <jacob.alzen@gmail.com>
2020-09-21 14:19:06 +0200

    Optimize berry to remove itself entirely on eaten

--- Enteties/EvolveBerry/EvolveBerry.gd
+++ Enteties/EvolveBerry/EvolveBerry.gd
@@ -1,10 +1,8 @@
 extends Area2D
 
 signal on_eaten
-var hidden = false
 
 func _on_EvolveBerry_body_entered(body):
-	if body.name == "Chameleon" && !hidden:
+	if body.name == "Chameleon":
 		emit_signal("on_eaten")
-		hidden = true
-		hide()
+		queue_free()

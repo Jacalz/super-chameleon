50cc0aeb04142b61daef7b444c7a48a15b8b58b5
Jacalz <jacob.alzen@gmail.com>
2020-09-24 10:58:04 +0200

    Update enemy to be killable

--- Enteties/Chameleon/evolve.gd
+++ Enteties/Chameleon/evolve.gd
@@ -2,6 +2,7 @@ extends Node2D
 
 onready var berries = get_tree().get_root().find_node("Berries", true, false)
 onready var timer = $Timer
+onready var player = $Unevolve
 
 signal on_evolving
 
@@ -12,3 +13,4 @@ func _ready():
 func evolve():
 	emit_signal("on_evolving")
 	timer.start()
+
--- Enteties/Enemies/Scorpion/scorpion.gd
+++ Enteties/Enemies/Scorpion/scorpion.gd
@@ -5,7 +5,6 @@ const SPEED = 100
 const GRAVITY = 1450
 
 var velocity = Vector2()
-var is_player = false
 
 onready var LeftD = $FloorDetector/Left
 onready var RightD = $FloorDetector/Right
@@ -21,3 +20,15 @@ func _physics_process(delta: float) -> void:
 		velocity.x *= -1
 	
 	velocity.y = move_and_slide(velocity, UP).y
+
+
+func _on_KillerInstinct_body_entered(body):
+	if body.name == "Chameleon":
+		if body.evolve_anim == "":
+				get_tree().reload_current_scene()
+		else:
+			if !is_on_wall():
+				 body.evolve_anim = ""
+			else:
+				queue_free()
+			

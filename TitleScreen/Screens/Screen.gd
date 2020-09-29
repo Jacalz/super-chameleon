08782d254adce138a0850b87764622c2dcde3e1d
Jacalz <jacob.alzen@gmail.com>
2020-09-29 14:31:08 +0200

    Fix menu issue and add "go back on enter"

--- TitleScreen/Screens/Screen.gd
+++ TitleScreen/Screens/Screen.gd
@@ -6,5 +6,6 @@ func _on_Button_pressed():
 func _on_PlayDesert_pressed():
 	get_tree().change_scene("res://Levels/Desert/Desert1.tscn")
 
-func _ready():
-	$CenterContainer/VBoxContainer/PlayDesert.grab_focus()
+func _physics_process(_delta):
+	if Input.is_action_just_pressed("ui_cancel"):
+		_on_Button_pressed()

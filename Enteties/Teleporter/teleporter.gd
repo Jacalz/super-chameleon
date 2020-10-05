cf456146365d6bc0d3980b3d53d9393a15eaf482
Jacalz <jacob.alzen@gmail.com>
2020-10-05 14:23:55 +0200

    Add background to teleporter and better foreground

--- Enteties/Teleporter/teleporter.gd
+++ Enteties/Teleporter/teleporter.gd
@@ -2,9 +2,6 @@ extends Area2D
 
 export(String, FILE, "*.tscn") var scene_to_load
 
-func _ready():
-	z_index = 10
-
 func _on_Teleporter_body_entered(body):
 	if body.name == "Chameleon":
 		get_tree().change_scene(scene_to_load)

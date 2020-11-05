eaaf9432cbaf362bde8ddcd7ca21feab41a04aad
Jacalz <jacob.alzen@gmail.com>
2020-11-05 11:33:10 +0100

    Fix issues with scorpion turning into a helicopter
    Both the gravity and horizontal speed was reversed leading to it taking off and flying like a helicopter.

--- Enteties/Enemies/Scorpion/scorpion.gd
+++ Enteties/Enemies/Scorpion/scorpion.gd
@@ -24,13 +24,13 @@ func _physics_process(delta: float) -> void:
 	velocity.y += GRAVITY * delta
 	
 	var need_turn = is_on_wall() or !LeftD.is_colliding() or !RightD.is_colliding()
-	var sees_left = PlayerL.is_colliding() and velocity.x == 100 and !player.hidden
-	var sees_right = PlayerR.is_colliding() and velocity.x == -100 and !player.hidden
+	var sees_left = velocity.x == SPEED and !player.hidden and PlayerL.is_colliding()
+	var sees_right =  velocity.x == -SPEED and !player.hidden and PlayerR.is_colliding()
 	
 	if sees_left or sees_right or need_turn:
-		velocity *= -1
+		velocity.x *= -1
 	
-	ASprite.flip_h = velocity.x == 100
+	ASprite.flip_h = velocity.x == SPEED
 	velocity.y = move_and_slide(velocity, UP).y
 
 

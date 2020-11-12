7c1139a056edb778f6475e2b784a3919c34181b0
Jacalz <jacob.alzen@gmail.com>
2020-11-12 11:08:21 +0100

    Open menu popup on input even fired instead of on physics thread

--- Enteties/Chameleon/menuPopup.gd
+++ Enteties/Chameleon/menuPopup.gd
@@ -12,9 +12,8 @@ func _on_Exit_pressed():
 	get_tree().change_scene("res://TitleScreen/TitleScreen.tscn")
 	get_tree().paused = false
 
-	
-func _physics_process(_delta):
-	if Input.is_action_just_pressed("ui_cancel"):
+func _input(event):
+	if Input.is_action_pressed("ui_cancel"):
 		if !get_tree().paused:
 			$VBoxContainer/Resume.grab_focus()
 			show()
